380b3bdca47cf08b6cc641cad57be17d072aae9e
Lina Battikha <lbattikha@ucsd.edu>
2023-10-05 15:44:22 -0700

    #5 completed

--- pset1/gpt.py
+++ pset1/gpt.py
@@ -15,6 +15,38 @@
 
 # 5
 
+import numpy as np
+
+def f(xi, yi, a, b):
+    return (yi - a * xi - b) ** 2
+
+def dL_db(x_vals, y_vals, a, b):
+    n = len(x_vals)
+    partial_sum = 0
+    
+    for i in range(n):
+        xi = x_vals[i]
+        yi = y_vals[i]
+        partial_sum += -2 * (yi - a * xi - b)
+    
+    return (1 / n) * partial_sum
+
+# Example usage:
+x_vals = np.array([1, 2, 3, 4, 5])
+y_vals = np.array([2, 4, 6, 8, 10])
+a = 2
+b = 1
+
+partial_derivative = dL_db(x_vals, y_vals, a, b)
+print("Partial derivative ∂L/∂b:", partial_derivative)
+
+# Link to chatGPT 3.5 conversation: https://chat.openai.com/share/c10bfe80-f1c5-48e3-9e7f-64b5508b8a84
+'''
+Correction to #5 - chatGPT 3.5 creating a new function for f but it was never
+used. Also, although not wrong, the code it wrote is longer than necessary. 
+'''
+
+
 # 6
 
 # 7 
@@ -64,6 +96,8 @@ def dL_db_gpt_4(x_vals, y_vals, a, b):
 # a = 1
 # b = 2
 # print(dL_db(x_vals, y_vals, a, b))
+
+# Link to chat conversation: https://chat.openai.com/share/c78cfcef-ed0c-4b8e-abea-cf794fa4f763
 
 
 # 6
