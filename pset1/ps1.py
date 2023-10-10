9ac260392a274c8b8c40510b8728a505ad22e395
oishani-b <147106992+oishani-b@users.noreply.github.com>
2023-10-09 21:34:22 -0700

    Update ps1.py
    problem 6

--- pset1/ps1.py
+++ pset1/ps1.py
@@ -8,29 +8,28 @@ from numpy.random import randn
 
 
 #Problem 1 - Oishani
-#hello
 def compute_slope_estimator(x_vals,y_vals):
 	n = len(x_vals)
-	x_bar = (np.sum(x_vals))/n
-	y_bar = (np.sum(y_vals))/n
+	x_bar = float((np.sum(x_vals))/n)
+	y_bar = float((np.sum(y_vals))/n)
 	numerator_sum = 0
 	for i in range(0,n):
 		prod = x_vals[i]*y_vals[i]
 		numerator_sum += prod
-	numerator = (numerator_sum - (n*x_bar*y_bar))
+	numerator = float(numerator_sum - (n*x_bar*y_bar))
 	denom_sum = 0
 	for i in range(0,n):
 		prod = x_vals[i]*x_vals[i]
 		denom_sum += prod
-	denominator = (denom_sum - (n*x_bar*x_bar)
+	denominator = float(denom_sum - (n*x_bar*x_bar))
 	a = numerator/denominator
 	return a		
 	
 
 #Problem 2 - Oishani
 def compute_intercept_estimator(x_vals,y_vals):
-	x_bar = (np.sum(x_vals))/n
-	y_bar = (np.sum(y_vals))/n
+	x_bar = float((np.sum(x_vals))/n)
+	y_bar = float((np.sum(y_vals))/n)
 	a = compute_slope_estimator(x_vals,y_vals)
 	b = y_bar - (a*x_bar)
 	return b
@@ -66,6 +65,11 @@ def dL_db(x_vals,y_vals,a,b):
 
 #Problem 6 - Oishani
 def gradient_descent_step(x_vals,y_vals,a,b,k=0.01):
+	dLda_over_n = dL_da(x_vals,y_vals,a,b)
+	a_updated = a - (k*dLda_over_n)
+	dLdb_over_n = dL_db(x_vals,y_vals,a,b)
+	b_updated = b - (k*dLda_over_n)
+	return (a_updated, b_updated)
 
 #Problem 7  - Kyra
 def gradient_descent(x_vals,y_vals,a_0=0,b_0=0,k=1000):
