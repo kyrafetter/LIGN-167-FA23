8df78fc69555d65ae8213ef0922eb6f4e335be54
Kyra Fetter <97578314+kyrafetter@users.noreply.github.com>
2023-11-18 18:10:48 -0800

    Update ps4.py

--- pset4/ps4.py
+++ pset4/ps4.py
@@ -1,1 +1,46 @@
+'''
+LIGN 167 PSET 4
+Kyra Fetter, A16819058
+Lina Battikha, A16852105
+Oishani Bandopadhyay, A16800427
+
+Group Member Contributions:
+Kyra: 9, 10, 11
+Lina: 1, 2, 3, 4
+Oishani: 5, 6, 7, 8 
+Everyone: Reviewing code and debugging
+'''
+
+# Problem 1 - Lina
+
+
+# Problem 2 - Lina
+
+
+# Problem 3 - Lina
+
+
+# Problem 4 - Lina
+
+
+# Problem 5 - Oishani
+
+
+# Problem 6 - Oishani
+
+
+# Problem 7 - Oishani
+
+
+# Problem 8 - Oishani
+
+
+# Problem 9 - Kyra
+
+
+# Problem 10 - Kyra
+
+
+# Problem 11 - Kyra
+
 
